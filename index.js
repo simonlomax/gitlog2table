03e90050cb06f590948bfb133453f0cc04693ea9
simon <simonwlomax@gmail.com>
2025-05-22 17:10:19 -0400

    v 1.0.0

--- index.js
+++ index.js
@@ -1,36 +1,85 @@
 import clipboard from 'clipboardy';
 import child_process from 'child_process';
 import { input, confirm } from '@inquirer/prompts';
+import ora from 'ora';
 
-const confirmation = await confirm({ message: "This command will git pull when executed. Is this ok?" });
+const subprocess = child_process;
+try {
+  const confirmation = await confirm({ message: "This command will git pull when executed. Is this ok?" });
 
-if (!confirmation) {
-  console.log('Exiting...');
-  process.exit(1);
-}
-
-const args = {
-  location: await input({ message: "Enter path to project directory: " }),
-  hashStart: await input({ message: "Enter starting commit hash: " }),
-  hashEnd: await input({ message: "Enter ending commit hash (or ..HEAD for last): " })
-}
-
-args.hashEnd === "" ? "..HEAD" : args.hashEnd
-
-
-const pullExec = `cd ${args.location} && git pull && git log --pretty=format:'%H|%s|%an' ${args.hashStart} ${args.hashEnd}`
-
-//const logExec = `git log --pretty=format:'%H|%s|%argsn' ${args.hashStart} ${args.hashEnd}`; 
-
-let exec = subprocess.exec(pullExec, (err, stdout, stderr) => {
-  let lines = stdout.toString().split('\n');
-  let markdown = '||**Hash**|**Description**|**Author**|\n|---|---|---|---|\n';
-  for (let i = 0; i < lines.length; i++) {
-    let line = lines[i].split('|');
-    markdown += '|' + (lines.length - i) + '|' + line[0].slice(0, 5) + '|' + line[1] + '|' + line[2] + '|\n';
+  if (!confirmation) {
+    console.log('Exiting...');
+    process.exit(1);
+  }
+  const args = {
+    location: await input({ message: "Enter path to project directory: " }),
+    hashStart: await input({ message: "Enter starting commit hash: " }),
+    hashEnd: await input({ message: "Enter ending commit hash (or HEAD for latest): " })
   }
 
-  clipboard.writeSync(markdown);
-  console.log('Table copied to clipboard');
-})
+  args.hashEnd === "" ? "HEAD" : args.hashEnd;
+  const pullExec = `cd ${args.location} && git pull`
+  const logExec = `cd ${args.location} && git log --pretty=format:'%H|%s|%an' ${args.hashStart}..${args.hashEnd}`;
+  const spinner = ora("Processing...").start();
+  try {
+    const result = await startProcess(pullExec, logExec);
+    spinner.succeed(result);
+  } catch (err) {
+    spinner.fail(err);
+  }
+
+} catch (error) {
+  if (error.name === "ExitPromptError") {
+    console.log("Exiting");
+    process.exit(1);
+  }
+}
+
+//args.hashEnd === "" ? "HEAD" : args.hashEnd;
+
+//const pullExec = `cd ${args.location} && git pull`
+//const logExec = `cd ${args.location} && git log --pretty=format:'%H|%s|%an' ${args.hashStart}..${args.hashEnd}`;
+
+function startProcess(pullExec, logExec) {
+  return new Promise((resolve, reject) => {
+
+    let exec = subprocess.exec(pullExec, (err, stdout, stderr) => {
+      if (stdout.toString() === undefined || stdout.toString() == "") {
+        reject("Directory does not have git initiated. Please check that it is correct.");
+      }
+      subprocess.exec(logExec, (err, stdout, stderr) => {
+        if (stdout.toString() === undefined || stdout.toString() === "") {
+          reject('Logging returned zero results. Check your hashes to make sure they are correct.');
+        }
+        let lines = stdout.toString().split('\n');
+        let markdown = '||**Hash**|**Description**|**Author**|\n|---|---|---|---|\n';
+        for (let i = 0; i < lines.length; i++) {
+          let line = lines[i].split('|');
+          markdown += '|' + (lines.length - i) + '|' + line[0].slice(0, 5) + '|' + line[1] + '|' + line[2] + '|\n';
+        }
+        clipboard.writeSync(markdown);
+        resolve('Table copied to clipboard');
+      });
+    });
+  })
+}
+
+/*const spinner = ora("Processing...").start();
+try {
+  const result = await startProcess();
+  spinner.succeed(result);
+} catch (err) {
+  spinner.fail(err);
+}
+
+process.on('uncaughtException', (error) => {
+  console.log("YOLO")
+
+  if (error.name === "ExitPromptError") {
+    console.log("cya.");
+  }
+  else {
+    throw error;
+  }
+});*/
 
